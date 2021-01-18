926af7dabb2a5fcfc6f509dfd8e072300ecfb619
yasukuro6259 <64528846+yasukuro6259@users.noreply.github.com>
2021-01-18 18:01:14 +0900

    Rubyドリルの1日目をコミット

---
+++ ruby_drill_day1.rb
@@ -0,0 +1,21 @@
+# 問題.1
+# puts hash.keys
+# puts hash.values
+# 上記のメソッドを実行した時に
+
+# one
+# two
+# three
+# 1
+# 2
+# 3
+
+# とターミナルに表示されるような変数hashを作成するためのコードをシンボルを使って記述してください。
+
+hash = {one:1, two:2, three:3}
+puts hash.keys
+puts hash.values
+
+# 変数 = {key1:value1, key2:value2, .....}
+# puts 変数.keys => キーを出力
+# puts 変数.values => バリューを出力
