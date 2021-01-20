11f90ed1305707af46170bf0dd2c3a257df3e71d
yasukuro6259 <64528846+yasukuro6259@users.noreply.github.com>
2021-01-21 07:10:13 +0900

    day6,7を修正

--- ruby_drill_day7.rb
+++ ruby_drill_day7.rb
@@ -1,45 +1,51 @@
 	
+# 問題.1
+# 以下の配列から、数を探して何番目に含まれているか結果を返すsearchメソッドをeach_with_indexを用いて作成してください。
+# input = [3, 5, 9 ,12, 15, 21, 29, 35, 42, 51, 62, 78, 81, 87, 92, 93]
 
-# def search(num, input)
-#   input.each_with_index  do |input_num, i|
-#     if input_num == num
-#       puts "#{i + 1}番目にあります。"
-#       return #ここでメソッドが完了、これがないとeachメソッドが実行され続ける
-#     else input_num != num
-#       puts "その数は含まれていません"
-#       return
-#     end
-#   end
-#   # puts "その数は含まれていません"
+# ヒント
+#  searchメソッド
+# （使用例）
+# search(12, input) => 4番目にあります
+# search(7, input) => その数は含まれていません
+
+# each_with_indexメソッド
+# each_with_indexはRubyに標準で組み込まれているメソッドの1つです。要素の繰り返し処理と同時に、その要素が何番目に処理されたのかも表すことができます。
+# 以下のように書きます。
+# 配列名.each_with_index  do |item, i|
 # end
 
-# input = [3, 5, 9 ,12, 15, 21, 29, 35, 42, 51, 62, 78, 81, 87, 92, 93]
-# search(3, input)
-
-def search(target_num, input)
-  input.each_with_index do |num, index|
-    if num == target_num
-      puts "#{index + 1}番目にあります"
-      return
-    end
-    puts "その数は含まれていません"
-    return
-  end
-end
-input = [3, 5, 9 ,12, 15, 21, 29, 35, 42, 51, 62, 78, 81, 87, 92, 93]
-search(11, input)
-
-# input = [3, 5, 9 ,12, 15, 21, 29, 35, 42, 51, 62, 78, 81, 87, 92, 93]
-# input.each_with_index  do |item, i|
-#   puts item
-#   puts i
-# end
-
+# 具体的には以下のように使うことができます。
 # fruits = ["メロン", "バナナ", "アップル"]
 # fruits.each_with_index do |item, i|
 #  puts "#{i}番目のフルーツは、#{item}です。"
 # end
 
+# これを実行すると、以下のような出力結果が得られます。
 # 0番目のフルーツは、メロンです。
 # 1番目のフルーツは、バナナです。
 # 2番目のフルーツは、アップルです。
+
+
+
+
+
+
+
+
+
+
+# 答え
+def search(selectNumber, input)
+    input.each_with_index do |number, i|
+      if selectNumber == number
+      return puts "#{i+1}番目にあります"
+      end
+    end
+    puts "その数は含まれていません。"
+end
+
+input = [3, 5, 9 ,12, 15, 21, 29, 35, 42, 51, 62, 78, 81, 87, 92, 93]
+puts "検索する数値を入力してください"
+num = gets.to_i
+search(num, input)
