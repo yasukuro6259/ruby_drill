a9ba765d60627a2bf15e2cbf0b5c472bffd40e05
yasukuro6259 <64528846+yasukuro6259@users.noreply.github.com>
2021-01-21 17:08:02 +0900

    day10の修正

--- ruby_drill_day10.rb
+++ ruby_drill_day10.rb
@@ -1,40 +1,51 @@
+# 問題.1
+# 以下のように、果物の名前と値段が入った配列があります。
+# この配列を用いて、果物の名前とそれぞれの合計額が出力される
+# コードを記述してください。
+
 # fruits_price = [["apple", [200, 250, 220]], ["orange", [100, 120, 80]], ["melon", [1200, 1500]]]
 
+# （出力）
+# appleの合計金額は670円です
+# orangeの合計金額は300円です
+# melonの合計金額は2700円です
 
-# fruits_price.each do |fruit_price|
-#   fruit_price[apple]
-# puts "appleの合計金額は#{}円です"
-# puts "orangeの合計金額は#{}円です"
-# puts "melonの合計金額は#{}円です"
 
-# fruits_price = [{fruit: "apple"{price: 200, 250, 220}},{fruit: "orange"{price: 100, 120, 80}}, {fruit:"melon"{price: 100, 120, 80}}]
-# puts fruits_price[0][:fruits][:price]
 
-# fruits_price = [{apple: {price: 200, 250, 220}}, {orange:{price:100, 120, 80}}, {melon: {price:1200, 1500}}]
-# puts fruits_price [0]
 
-# fruits_price = [{apple: {price: [200, 100, 250]}}, {orange:{price:100, 120, 80}}, {melon: {price:1200, 1500}}]
-# puts fruits_price [0][:apple][:price]
 
+
+
+
+
+
+
+
+
+# 答え
+def get_sum_price(fruits_prices)
+  fruits_prices.each do |fruits_price|
+    name=fruits_price[0]
+    prices=fruits_price[1]
+    sum = 0
+    prices.each do |price|
+      sum += price
+    end
+    puts "#{name}の合計金額は#{sum}円です"
+  end
+end
+
+fruits_price = [["apple", [200, 250, 220]], ["orange", [100, 120, 80]], ["melon", [1200, 1500]]]
+get_sum_price(fruits_price)
+
+
+# 模範回答
+# fruits_price = [["apple", [200, 250, 220]], ["orange", [100, 120, 80]], ["melon", [1200, 1500]]]
 
 # fruits_price.each do |fruit|
 #   sum = 0
-#   fruit[:apple][:price].each do |price|
+#   fruit[1].each do |price|
 #     sum += price
 #   end
-#   puts sum
-#   return
-# end これだとappleだけしか出力できない。
-
-fruits_price = ["apple" {price: [200, 100, 250]}, "orange":{price:[100, 120, 80]}, {melon: {price:[1200, 1500]}]
-puts fruits_price [0][:apple][:price]
-
-fruits_price = [["apple", [200, 250, 220]], ["orange", [100, 120, 80]], ["melon", [1200, 1500]]]
-
-fruits_price.each do |fruit|
-  sum = 0
-  fruit[1].each do |price|
-    sum += price
-  end
-  puts "#{fruit[0]}の合計金額は#{sum}円です"
-end
+#   puts "#{fruit[0]}の合計金額は#{sum}円です"
+# end
