fd5909caf577867e92c50593208f229bc8d60b52
yasukuro6259 <64528846+yasukuro6259@users.noreply.github.com>
2021-01-22 07:12:31 +0900

    day13の実施

--- ruby_drill_day13.rb
+++ ruby_drill_day13.rb
@@ -1,23 +1,49 @@
-# # 配列を作成します。
-# array = [0,1,2,3,4,5,6]
+# 問題.1
+# 任意の文字に対してn番目の文字を消し、
+# その消した文字を出力するメソッドを作りましょう。
 
-# # 配列から引数で指定した要素を slice します。
-# ele = array.slice(1)
+# 呼び出し方
+# missing_char(string, num)
+# 出力例：
+# missing_char('kitten', 1) → 'itten'
+# missing_char('kitten', 2) → 'ktten'
+# missing_char('kitten', 4) → 'kiten'
+# ヒント
+# sliceメソッドを用いることで、配列や文字列から指定した要素を取り出すことができます。
+# 文字列の要素を指定する際は数字を用い、先頭の文字列は0からカウントされます。
 
-# # 引数で指定した要素が slice されました。
-# puts ele
-# #=> 1
+# 文字列を作成
+# string = "abcdefg"
 
-# # 配列はもとのままです。
-# puts array #=> [0,1,2,3,4,5,6]
+# 文字列から引数で指定した要素を取得して変数に代入
+# str = string.slice(2)
 
-#呼び出し方
+# strに代入した文字列を出力
+# puts str => "c"
+
+# 文字列はもとのまま
+# puts string => "abcdefg"
+#  slice!
+# 末尾にエクスクラメーションマーク(!)のつくメソッドを破壊的メソッドといいます。これはもとの配列や文字列を変化させるメソッドです。slice!メソッドを使用することで、指定した要素を削除することができます。
+# slice!メソッドの詳細は公式ドキュメントを確認しましょう。
+
+# string = "abcdefg"
+# str = string.slice!(2)
+# puts str => "c"
+# #"c"が取り除かれている
+# puts string => "abdefg"
+
+
+
+# 任意の文字に対してn番目の文字を消し、
+# その消した文字を出力するメソッドを作りましょう。
 def missing_char(string, num)
-  ele = string.slice!(num - 1)
-  puts string
+  str = string.slice!(num-1)
 end
 
-missing_char('kitten', 1)
-missing_char('kitten', 2)
-missing_char('kitten', 4)
-
+puts "文字を入力してください"
+text = gets.to_s
+puts "何文字目を消しますか？"
+num = gets.to_i
+delete_char=missing_char(text, num)
+puts "消した文字は#{delete_char}です。"
