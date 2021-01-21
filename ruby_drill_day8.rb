8cd97c054e2e96ca456350b856f61489116c06ce
yasukuro6259 <64528846+yasukuro6259@users.noreply.github.com>
2021-01-21 09:48:44 +0900

    day8の修正

--- ruby_drill_day8.rb
+++ ruby_drill_day8.rb
@@ -1,23 +1,35 @@
-# puts "登録したい名前を入力してください"
-# name = gets.chomp
-# if name.include?(" ")
-#   puts "#{name}->!エラー!空白は登録できません"
-# elsif name.include?(".")
-#   puts "#{name}->!エラー!記号は登録できません"
-# else
-#   puts "#{name}->登録完了しました"
-# end
+# 問題.1
+# 名前を入力する機能を作成しましょう。
+# 名前以外にピリオド(.)や空白があるとエラーを表示させます。
 
-def check_name(name)
-  if name.include?(" ")
-    puts "#{name}->!エラー!空白は登録できません"
-  elsif name.include?(".")
-    puts "#{name}->!エラー!記号は登録できません"
+# 出力例：
+# 登録したい名前を入力してください(例)YamadaTaro
+# YamadaTaro → 登録が完了しました
+# Yamada.Taro→!エラー!記号は登録できません
+# Yamada Taro → !エラー!空白は登録できません
+
+# ヒント
+# include?メソッドを使いましょう。
+# include?メソッドは指定した要素が、配列や文字列内に含まれているかを判定するメソッドです。
+# include?メソッドの詳細は公式リファレンスを確認しましょう。
+
+# array = ["foo", "bar"]
+# puts array.include?("bar") => true
+# puts array.include?("hoge") => false
+
+
+def nameCheck(inputName)
+  if inputName.include?(".")
+    puts "エラー!記号は登録できません"
+  elsif inputName.include?(" ")
+    puts "エラー！空白は登録できません"
   else
-    puts "#{name}->登録完了しました"
+    puts "登録が完了しました。"
   end
 end
 
-puts "登録したい名前を入力してください"
-name = gets
-check_name(name)
+puts "名前を入力してください"
+name = gets.to_s
+nameCheck(name)
+
+
