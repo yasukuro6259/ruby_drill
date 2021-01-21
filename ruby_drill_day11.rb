7d31630839833d18ce95d946649013aa86d82f43
yasukuro6259 <64528846+yasukuro6259@users.noreply.github.com>
2021-01-21 17:28:36 +0900

    day11の修正

--- ruby_drill_day11.rb
+++ ruby_drill_day11.rb
@@ -3,25 +3,29 @@
 # 数値が0以下、または11以上であってもTrueを出力します。
 # それ以外はFalseを出力するメソッドを論理演算子&&と||を用いて作りましょう。
 
+# def in1to10(num, outside_mode)
+#   # ここに条件式を記述する
+# end
+
 # 出力例：
 # in1to10(5,false) →True
 # in1to10(11,false) →False
 # in1to10(11,true) →True
 
+
+
+
+
+
+
+
+
+
+#答え
 def in1to10(num, outside_mode)
-  # ここに条件式を記述する
-end
-
-
-
-
-
-puts "数値を入力"
-num = gets.to_i
-if num >= 1 && num <=  10
-  puts "True"
-elsif num <= 0 || num >= 11
-  puts "True"
-else
-  puts "False"
+  if (num>=1 && num<=10) || outside_mode
+    puts "True"
+  else
+    puts "False"
+  end
 end
